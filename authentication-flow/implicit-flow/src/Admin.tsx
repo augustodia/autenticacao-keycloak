753472a5906ddd88ad656a075f6142001663bfeb
Luiz Augusto <augustodia97@gmail.com>
2023-07-03 17:47:46 -0300

    ✨ create logout button

--- authentication-flow/implicit-flow/src/Admin.tsx
+++ authentication-flow/implicit-flow/src/Admin.tsx
@@ -3,14 +3,19 @@ import {AuthContext} from "./AuthProvider.tsx";
 import {makeLoginUrl} from "./utils.ts";
 
 export function Admin() {
-    const {auth} = useContext(AuthContext);
+    const {auth,makeLogoutUrl} = useContext(AuthContext);
 
     useEffect(() => {
         if(!auth) window.location.href = makeLoginUrl();
     }, [auth])
 
+    function logout() {
+        window.location.href = makeLogoutUrl();
+    }
+
     return (
         <div>
+            <button onClick={logout}>Logout</button>
             <h1>Admin</h1>
             <pre >{JSON.stringify(auth)}</pre>
         </div>
